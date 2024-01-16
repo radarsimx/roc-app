42ee6961644eac8c5849dbe741055149af5cbf0f
Zhengyu Peng <rookiepeng@users.noreply.github.com>
2024-01-16 15:26:41 -0500

    add docstring

--- layout/layout.py
+++ layout/layout.py
@@ -27,6 +27,8 @@
 
 """
 
+import uuid
+
 from dash import dcc
 from dash import html
 
@@ -34,117 +36,157 @@ import dash_bootstrap_components as dbc
 
 import plotly.io as pio
 
-import uuid
+colorscales = [
+    "Blackbody",
+    "Bluered",
+    "Blues",
+    "Earth",
+    "Electric",
+    "Greens",
+    "Greys",
+    "Hot",
+    "Jet",
+    "Picnic",
+    "Portland",
+    "Rainbow",
+    "RdBu",
+    "Reds",
+    "Viridis",
+    "YlGnBu",
+    "YlOrRd",
+]
 
-colorscales = ['Blackbody',
-               'Bluered',
-               'Blues',
-               'Earth',
-               'Electric',
-               'Greens',
-               'Greys',
-               'Hot',
-               'Jet',
-               'Picnic',
-               'Portland',
-               'Rainbow',
-               'RdBu',
-               'Reds',
-               'Viridis',
-               'YlGnBu',
-               'YlOrRd']
-
-INTEGRATION = ['Swerling 0',
-               'Swerling 1',
-               'Swerling 2',
-               'Swerling 3',
-               'Swerling 4',
-               'Coherent']
+INTEGRATION = [
+    "Swerling 0",
+    "Swerling 1",
+    "Swerling 2",
+    "Swerling 3",
+    "Swerling 4",
+    "Coherent",
+]
 
 
-card_gain = dbc.Card([
-    dbc.CardBody([
-        dbc.Row([
-            dbc.Col(dbc.Row([
-                dbc.FormText("Pd, probability of detection"),
-                dbc.Col(dbc.Input(id='pd',
-                                  type="number",
-                                  value=0.5,
-                                  min=0.01,
-                                  max=0.9999,
-                                  step=0.0001,
-                                  className="mb-3")),
-
-                dbc.FormText("Pfa, probability of false alarm"),
-                dbc.Col(dbc.Input(id='pfa',
-                                  type="number",
-                                  value=0.0001,
-                                  min=0.00000001,
-                                  max=0.1,
-                                  step=0.00000001,
-                                  className="mb-3")),
-
-                dbc.FormText("N, number of channels"),
-                dcc.Slider(
-                    id='channels',
-                    min=1,
-                    max=1024,
-                    step=1,
-                    value=128,
-                    marks=None,
-                    tooltip={'always_visible': True,
-                             'placement': 'bottom'}
-                ),
-
-                dbc.FormText("Types of targets", className="mt-3"),
-                dcc.Dropdown(
-                    id='integration',
-                    options=[{'label': i,
-                              'value': i}
-                             for i in INTEGRATION],
-                    value=['Swerling 1', 'Swerling 3'],
-                    multi=True
-                ),
-
-                dbc.Col(html.Hr()),
-                dbc.Label("Single Channel Minimal SNR"),
-
-                dbc.Spinner(
-                    dbc.Row(id='minsnr-container', children=[]),
-                    color="primary",
-                    type="grow"
-                ),
-
-            ]), width=3),
-            dbc.Col(
-                dbc.Spinner(
-                    dcc.Graph(
-                        id='scatter',
-                        figure={
-                            'data': [{'mode': 'lines',
-                                      'type': 'scatter',
-                                      'x': [],
-                                      'y': []}],
-                            'layout': {'template': pio.templates['plotly'],
-                                       'height': 700,
-                                       'uirevision': 'no_change',
-                                       'xaxis': dict(title='Number of Channels'),
-                                       'yaxis': dict(title='Integration Gain (dB)')}
-                        },
-                    ), color="primary", type="grow"), width=9),])
-    ]),
-], className="shadow-sm",
+card_gain = dbc.Card(
+    [
+        dbc.CardBody(
+            [
+                dbc.Row(
+                    [
+                        dbc.Col(
+                            dbc.Row(
+                                [
+                                    dbc.FormText("Pd, probability of detection"),
+                                    dbc.Col(
+                                        dbc.Input(
+                                            id="pd",
+                                            type="number",
+                                            value=0.5,
+                                            min=0.01,
+                                            max=0.9999,
+                                            step=0.0001,
+                                            className="mb-3",
+                                        )
+                                    ),
+                                    dbc.FormText("Pfa, probability of false alarm"),
+                                    dbc.Col(
+                                        dbc.Input(
+                                            id="pfa",
+                                            type="number",
+                                            value=0.0001,
+                                            min=0.00000001,
+                                            max=0.1,
+                                            step=0.00000001,
+                                            className="mb-3",
+                                        )
+                                    ),
+                                    dbc.FormText("N, number of channels"),
+                                    dcc.Slider(
+                                        id="channels",
+                                        min=1,
+                                        max=1024,
+                                        step=1,
+                                        value=128,
+                                        marks=None,
+                                        tooltip={
+                                            "always_visible": True,
+                                            "placement": "bottom",
+                                        },
+                                    ),
+                                    dbc.FormText("Types of targets", className="mt-3"),
+                                    dcc.Dropdown(
+                                        id="integration",
+                                        options=[
+                                            {"label": i, "value": i}
+                                            for i in INTEGRATION
+                                        ],
+                                        value=["Swerling 1", "Swerling 3"],
+                                        multi=True,
+                                    ),
+                                    dbc.Col(html.Hr()),
+                                    dbc.Label("Single Channel Minimal SNR"),
+                                    dbc.Spinner(
+                                        dbc.Row(id="minsnr-container", children=[]),
+                                        color="primary",
+                                        type="grow",
+                                    ),
+                                ]
+                            ),
+                            width=3,
+                        ),
+                        dbc.Col(
+                            dbc.Spinner(
+                                dcc.Graph(
+                                    id="scatter",
+                                    figure={
+                                        "data": [
+                                            {
+                                                "mode": "lines",
+                                                "type": "scatter",
+                                                "x": [],
+                                                "y": [],
+                                            }
+                                        ],
+                                        "layout": {
+                                            "template": pio.templates["plotly"],
+                                            "height": 700,
+                                            "uirevision": "no_change",
+                                            "xaxis": {"title": "Number of Channels"},
+                                            "yaxis": {"title": "Integration Gain (dB)"},
+                                        },
+                                    },
+                                ),
+                                color="primary",
+                                type="grow",
+                            ),
+                            width=9,
+                        ),
+                    ]
+                )
+            ]
+        ),
+    ],
+    className="shadow-sm",
 )
 
 
 def get_app_layout():
-    return dbc.Container([
-        dcc.Store(id='session-id', data=str(uuid.uuid4())),
+    """
+    Get the layout for the Dash web application.
 
-        dbc.Row([dbc.Col(card_gain)], className='my-3'),
-
-        html.Hr(),
-
-        dcc.Markdown(
-            'v1.0 | Powered by [Dash](https://plotly.com/dash/)'),
-    ], fluid=True, className="dbc_light")
+    Returns:
+    dbc.Container: Dash Bootstrap container containing the layout elements.
+    - dcc.Store: Dash Core Component for storing session ID data.
+    - dbc.Row: Dash Bootstrap row containing a column with a card (assumed to be defined elsewhere as 'card_gain').
+    - html.Hr: Dash HTML Horizontal Rule for visual separation.
+    - dcc.Markdown: Dash Core Component for rendering Markdown text.
+    """
+    return dbc.Container(
+        [
+            dcc.Store(id="session-id", data=str(uuid.uuid4())),
+            dbc.Row([dbc.Col(card_gain)], className="my-3"),
+            html.Hr(),
+            dcc.Markdown("v1.0 | Powered by [Dash](https://plotly.com/dash/)"),
+        ],
+        fluid=True,
+        className="dbc_light",
+    )
--- roc_app.py
+++ roc_app.py
@@ -42,80 +42,107 @@ from flaskwebgui import FlaskUI
 
 from layout.layout import get_app_layout
 
-app = dash.Dash(__name__,
-                meta_tags=[{
-                    'name': 'viewport',
-                    'content': 'width=device-width,initial-scale=1'
-                }]
-                )
+app = dash.Dash(
+    __name__,
+    meta_tags=[{"name": "viewport", "content": "width=device-width,initial-scale=1"}],
+)
 
 app.scripts.config.serve_locally = True
 app.css.config.serve_locally = True
-app.title = 'ROC'
+app.title = "ROC"
 app.layout = get_app_layout
 server = app.server
 
 
 @app.callback(
-    output=dict(
-        fig=Output('scatter', 'figure'),
-        minsnr_container=Output('minsnr-container', 'children'),
-    ),
-    inputs=dict(
-        pd=Input('pd', 'value'),
-        pfa=Input('pfa', 'value'),
-        n=Input('channels', 'value'),
-        model=Input('integration', 'value'),
-    ),
-    state=dict(
-        min_pd=State('pd', 'min'),
-        max_pd=State('pd', 'max'),
-        min_pfa=State('pfa', 'min'),
-        max_pfa=State('pfa', 'max'),
-    )
+    output={
+        "fig": Output("scatter", "figure"),
+        "minsnr_container": Output("minsnr-container", "children"),
+    },
+    inputs={
+        "pd": Input("pd", "value"),
+        "pfa": Input("pfa", "value"),
+        "n": Input("channels", "value"),
+        "model": Input("integration", "value"),
+    },
+    state={
+        "min_pd": State("pd", "min"),
+        "max_pd": State("pd", "max"),
+        "min_pfa": State("pfa", "min"),
+        "max_pfa": State("pfa", "max"),
+    },
 )
 def gain_plot(pd, pfa, n, model, min_pd, max_pd, min_pfa, max_pfa):
+    """
+    Generate a plot for integration gain based on probability of detection (Pd), probability of false alarm (Pfa),
+    number of channels (n), and a list of models.
+
+    Parameters:
+    - pd (float): Probability of detection.
+    - pfa (float): Probability of false alarm.
+    - n (int): Number of channels.
+    - model (list): List of models.
+    - min_pd (float): Minimum value for Pd.
+    - max_pd (float): Maximum value for Pd.
+    - min_pfa (float): Minimum value for Pfa.
+    - max_pfa (float): Maximum value for Pfa.
+
+    Raises:
+    - PreventUpdate: If pd is None, pd is outside the range [min_pd, max_pd],
+                    pfa is None, or pfa is outside the range [min_pfa, max_pfa].
+
+    Returns:
+    dict: A dictionary containing the plot data and layout, as well as minsnr_container information.
+    - fig (dict): Plotly figure data and layout.
+    - minsnr_container (list): List of FormText containing minimum SNR information for each model.
+    """
     if pd is None:
         raise PreventUpdate
-    elif pd < min_pd or pd > max_pd:
+    if pd < min_pd or pd > max_pd:
         raise PreventUpdate
 
     if pfa is None:
         raise PreventUpdate
-    elif pfa < min_pfa or pfa > max_pfa:
+    if pfa < min_pfa or pfa > max_pfa:
         raise PreventUpdate
 
-    n_array = np.arange(1, n+1)
+    n_array = np.arange(1, n + 1)
     nci_gain = np.zeros((len(model), n), dtype=np.float64)
     fig_data = []
     minsnr_container = []
     for m_idx, mod in enumerate(model):
         minsnr = roc_snr(pfa, pd, 1, mod)
-        minsnr_container.append(dbc.FormText(
-            mod+': '+str(round(minsnr, 3))+' dB'))
-        for idx in range(1, n+1):
-            nci_gain[m_idx, idx-1] = minsnr - \
-                roc_snr(pfa, pd, n_array[idx-1], mod)
+        minsnr_container.append(
+            dbc.FormText(mod + ": " + str(round(minsnr, 3)) + " dB")
+        )
+        for idx in range(1, n + 1):
+            nci_gain[m_idx, idx - 1] = minsnr - roc_snr(pfa, pd, n_array[idx - 1], mod)
         fig_data.append(
-            {'mode': 'lines',
-             'type': 'scatter',
-             'x': n_array,
-             'y': nci_gain[m_idx, :],
-             'name': mod}
+            {
+                "mode": "lines",
+                "type": "scatter",
+                "x": n_array,
+                "y": nci_gain[m_idx, :],
+                "name": mod,
+            }
         )
 
-    return dict(
-        fig={'data': fig_data,
-             'layout': {'template': pio.templates['plotly'],
-                        'height': 700,
-                        'uirevision': 'no_change',
-                        'title': 'Pd = '+str(pd)+', Pfa = '+str(pfa),
-                        'xaxis': dict(title='Number of Channels'),
-                        'yaxis': dict(title='Integration Gain (dB)')}
-             },
-        minsnr_container=minsnr_container)
+    return {
+        "fig": {
+            "data": fig_data,
+            "layout": {
+                "template": pio.templates["plotly"],
+                "height": 700,
+                "uirevision": "no_change",
+                "title": "Pd = " + str(pd) + ", Pfa = " + str(pfa),
+                "xaxis": {"title": "Number of Channels"},
+                "yaxis": {"title": "Integration Gain (dB)"},
+            },
+        },
+        "minsnr_container": minsnr_container,
+    }
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # app.run_server(debug=True, threaded=True, processes=1, host='0.0.0.0')
     FlaskUI(app=server, server="flask").run()
